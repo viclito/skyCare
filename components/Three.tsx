884c284e3db5fd3e637b4f27bab8d39813f206a4
viclito <berglin1998@gmail.com>
2024-03-18 11:44:41 +0530

    first commit

--- components/Three.tsx
+++ components/Three.tsx
@@ -1,42 +1,13 @@
-// "use client"
-// import React from 'react';
-// import { Suspense } from 'react';
-// import { Canvas } from '@react-three/fiber';
-// import { Environment, OrbitControls } from '@react-three/drei';
-// import Untitled from '../public/Cargo';
-
-
-
-// // Convert functional component to TypeScript
-// const Three = () => {
-//   return (
-//     <>
-//       <Canvas className='h-[700px] md:h-[400px] xs:h-[300px]'>
-//         <ambientLight intensity={1.5} />
-//         <OrbitControls enableZoom={false}/>
-//         <Suspense fallback={null}>
-//           <Untitled />
-//         </Suspense>
-//         <Environment preset="sunset" />
-//       </Canvas>
-//     </>
-//   );
-// };
-
-// export default Three;
+"use client"
 import React from 'react';
 import { Suspense } from 'react';
-import { Canvas, useLoader } from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import { Environment, OrbitControls } from '@react-three/drei';
-import { GLTFLoader } from 'three/examples/jsm/Addons.js';
+import Untitled from '../public/Cargo';
+
+
 
 // Convert functional component to TypeScript
-const Cargo = () => {
-  const gltf = useLoader(GLTFLoader, '/Cargo.gltf');
-
-  return <primitive object={gltf.scene} />;
-};
-
 const Three = () => {
   return (
     <>
@@ -44,7 +15,7 @@ const Three = () => {
         <ambientLight intensity={1.5} />
         <OrbitControls enableZoom={false}/>
         <Suspense fallback={null}>
-          <Cargo />
+          <Untitled />
         </Suspense>
         <Environment preset="sunset" />
       </Canvas>
